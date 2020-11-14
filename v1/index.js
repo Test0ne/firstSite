f335609269c30a11c69d34c245683a3c687899a5
Test0ne <stratacraftowner@hotmail.com>
2020-11-13 18:06:29 -0600

    ;

--- v1/index.js
+++ v1/index.js
@@ -38,17 +38,17 @@ mongoose.connect('mongodb://localhost:27017/firstSite', {
 
 
 //Middleware/config
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 app.use(cookieParser(secretTemp));
-app.use(session(sessionConfig))
+app.use(session(sessionConfig));
 app.use(morgan('dev'));
-app.use(setUser)
-app.use(flash())
-app.engine('ejs',ejsMate)
-app.set('views', path.join(__dirname, '/views'))
+app.use(setUser);
+app.use(flash());
+app.engine('ejs',ejsMate);
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 app.use((req,res,next) => {
     res.locals.data = req.flash('data');
@@ -60,48 +60,48 @@ app.use((req,res,next) => {
 
 
 //Routes
-const storeRoutes = require('./routes/store')
-const postRoutes = require('./routes/posts')
-const testRoutes = require('./routes/tests')
-app.use('/store',storeRoutes)
-app.use('/post',postRoutes)
-app.use('',testRoutes)
+const storeRoutes = require('./routes/store');
+const postRoutes = require('./routes/posts');
+const testRoutes = require('./routes/tests');
+app.use('/store',storeRoutes);
+app.use('/post',postRoutes);
+app.use('',testRoutes);
 
 
 //home page
 app.get('/', (req, res) => {
-    res.render('home',{title:"Home"})
+    res.render('home',{title:"Home"});
 });
 app.get('/about', (req, res) => {
-    res.render('about',{title:"About us"})
+    res.render('about',{title:"About us"});
 });
 
 
 //====USER MANAGEMENT START
     app.get('/register', (req, res) => {
-        res.render('register', {title:"Sign up"})
+        res.render('register', {title:"Sign up"});
     });
     app.post('/register', (req, res) => {
-        res.render('register', {title:"Sign up"})
+        res.render('register', {title:"Sign up"});
     });
     app.get('/login', (req, res) => {
-        res.render('login', {title:"Sign in"})
+        res.render('login', {title:"Sign in"});
     });
 //====USER MANAGEMENT END
 
 
 //====TV SEARCH API START
     app.get('/shows', (req, res) => {
-        hDebug("/shows get")
-        res.render('shows',{title:"Shows"})
+        hDebug("/shows get");
+        res.render('shows',{title:"Shows"});
     }); 
     app.post('/shows', wrapAsync(async (req, res) => {
-        console.log("Show test!")
-        console.dir(req.body)
+        console.log("Show test!");
+        console.dir(req.body);
         const shows = await axios.get("http://api.tvmaze.com/search/shows?q="+req.body.search);
         req.flash('success','Search results received!');
-        req.flash('shows',shows.data)
-        res.redirect('/shows')
+        req.flash('shows',shows.data);
+        res.redirect('/shows');
     }));
 //====TV SEARCH API END
 
@@ -109,12 +109,12 @@ app.get('/about', (req, res) => {
 //====Uncatgorized
     //GAME PAGE
     app.get('/game', (req, res) => {
-        res.render('game',{title:"Game"})
+        res.render('game',{title:"Game"});
     });
     //Catch invalid
     app.all('*', (req,res,next)=>{
-        next(new exError(404,"Page not found!"))
-    })
+        next(new exError(404,"Page not found!"));
+    });
 //====Uncatgorized
 
 
@@ -123,10 +123,10 @@ app.use((err, req, res, next) => {
     const { status = 500 } = err;
     const { message = "Something went wrong.\nERROR: "} = err;
     //hError("ERROR! "+err)
-    hError("ERROR MSG: "+message)
-    hError("ERROR STATUS: "+status)
-    res.render('error',{title: status+' error!',status,message})
-})
+    hError("ERROR MSG: "+message);
+    hError("ERROR STATUS: "+status);
+    res.render('error',{title: status+' error!',status,message});
+});
 
 
 app.listen(3000, () => {hInfo("LISTENING ON PORT 3000")});
